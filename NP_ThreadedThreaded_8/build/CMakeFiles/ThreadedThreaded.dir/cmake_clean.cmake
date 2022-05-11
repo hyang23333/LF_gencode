f9ab3d951c8fed633fc1ded82fcdca6092679526
hyang23333 <m13210889156@outlook.com>
2022-05-10 22:12:37 -0700

    weak scaling

---
+++ NP_ThreadedThreaded_8/build/CMakeFiles/ThreadedThreaded.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ThreadedThreaded.dir/ThreadedThreaded.c.o"
+  "CMakeFiles/ThreadedThreaded.dir/ThreadedThreaded.c.o.d"
+  "CMakeFiles/ThreadedThreaded.dir/core/mixed_radix.c.o"
+  "CMakeFiles/ThreadedThreaded.dir/core/mixed_radix.c.o.d"
+  "CMakeFiles/ThreadedThreaded.dir/core/platform/lf_macos_support.c.o"
+  "CMakeFiles/ThreadedThreaded.dir/core/platform/lf_macos_support.c.o.d"
+  "CMakeFiles/ThreadedThreaded.dir/core/threaded/scheduler_NP.c.o"
+  "CMakeFiles/ThreadedThreaded.dir/core/threaded/scheduler_NP.c.o.d"
+  "CMakeFiles/ThreadedThreaded.dir/core/utils/semaphore.c.o"
+  "CMakeFiles/ThreadedThreaded.dir/core/utils/semaphore.c.o.d"
+  "CMakeFiles/ThreadedThreaded.dir/ctarget/schedule.c.o"
+  "CMakeFiles/ThreadedThreaded.dir/ctarget/schedule.c.o.d"
+  "CMakeFiles/ThreadedThreaded.dir/ctarget/util.c.o"
+  "CMakeFiles/ThreadedThreaded.dir/ctarget/util.c.o.d"
+  "ThreadedThreaded"
+  "ThreadedThreaded.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ThreadedThreaded.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
